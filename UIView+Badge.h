d89b75481568d829b1fc10dd39a1852a41a81be2
chisj <chisj2012@gmail.com>
2013-03-15 10:33:43 +0800

    first init

---
+++ UIView+Badge.h
@@ -0,0 +1,17 @@
+//
+//  UIView+Badge.h
+//  GetBadgeDemo
+//
+//  Created by csj on 13-3-15.
+//  Copyright (c) 2013年 eu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface  UIView (UIView_Badge) {
+
+}
+
+- (UIView *)showBadgeValue:(NSString *)strBadgeValue;
+- (void)removeBadgeValue;
+@end
